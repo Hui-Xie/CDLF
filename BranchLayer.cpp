4f0672947019690fac477ccfed519e0d6f2ad9e6
Hui Xie <Hui-Xie@uiowa.edu>
2018-08-04 11:22:05 -0500

    add Branch Layer

---
+++ BranchLayer.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Sheen156 on 8/4/2018.
+//
+
+#include "BranchLayer.h"
+
+BranchLayer::BranchLayer(const int id, const string& name, const vector<int>& tensorSize, Layer *prevLayer): Layer(id, name, tensorSize)
+{
+    m_type = "BranchLayer";
+    addPreviousLayer(prevLayer);
+}
+
+BranchLayer::~BranchLayer()
+{
+    //null
+}
+
+void BranchLayer::initialize(const string& initialMethod){
+    //null
+}
+
+void BranchLayer::zeroParaGradient(){
+    //null
+}
+
+void BranchLayer::forward(){
+    *m_pYTensor = *(m_prevLayer->m_pYTensor);
+}
+
+void BranchLayer::backward(){
+    // m_pdYTensor has been initialize to zero
+    *(m_prevLayer->m_pdYTensor) += *(m_pdYTensor);
+}
+
+void BranchLayer::updateParameters(const float lr, const string& method, const int batchSize){
+    //null
+}
+
+void BranchLayer::addNextLayer(Layer* nextLayer){
+    if (nullptr != nextLayer){
+        m_nextLayers.push_back(nextLayer);
+    }
+}
---
+++ BranchLayer.h
@@ -0,0 +1,33 @@
+//
+// Created by Sheen156 on 8/4/2018.
+//
+
+#ifndef RL_NONCONVEX_BRANCHLAYER_H
+#define RL_NONCONVEX_BRANCHLAYER_H
+
+#include "Layer.h"
+
+/* Y_i = X
+ * just mirror input X into a lot of replicated Y_i
+ * dL/dX = \sum dL/dY
+ * */
+
+class BranchLayer : public Layer {
+public:
+    BranchLayer(const int id, const string& name, const vector<int>& tensorSize, Layer *prevLayer);
+    ~BranchLayer();
+
+    list<Layer*> m_nextLayers;
+
+    virtual  void initialize(const string& initialMethod);
+    virtual  void zeroParaGradient();
+    virtual  void forward();
+    virtual  void backward();
+    virtual  void updateParameters(const float lr, const string& method, const int batchSize=1);
+
+    void addNextLayer(Layer* nextLayer);
+};
+
+
+
+#endif //RL_NONCONVEX_BRANCHLAYER_H
