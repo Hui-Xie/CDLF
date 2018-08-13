1d33c9f75b416ec03a5b1b3365b812ba4570b34d
Hui Xie <Hui-Xie@uiowa.edu>
2018-08-13 16:22:37 -0500

    build network of convolution test

--- examples/ConvolutionTest/ConvolutionTest.cpp
+++ examples/ConvolutionTest/ConvolutionTest.cpp
@@ -1,4 +1,39 @@
 //
 // Created by Hui Xie on 8/13/2018.
 //
+#include "CDLF.h"
 
+/*  inputLayer: 5*5
+ *  ConvLayer: 1 filter of size 3*3;
+ *  VectorizationLayer: 9*1
+ *  LossConvexExmaple1: 1
+ *
+ *  * */
+
+#include "LossConvexExample1.h"
+
+int main (int argc, char *argv[])
+{
+    Net net;
+
+    // build network
+    int id =1;
+    InputLayer* inputLayer = new InputLayer(id++, "InputLayer", {5,5});
+    net.addLayer(inputLayer);
+
+    ConvolutionLayer* conv1 = new ConvolutionLayer(id++, "Conv1", {3,3}, net.getFinalLayer());
+    net.addLayer(conv1);
+
+    VectorizationLayer* vec1 = new VectorizationLayer(id++, "Vec1", net.getFinalLayer());
+    net.addLayer(vec1);
+
+    LossConvexExample1* loss = new LossConvexExample1(id++, "Loss", net.getFinalLayer());
+    net.addLayer(loss);
+    // run network
+
+
+
+
+    cout<< "End of Convolution Test."<<endl;
+    return 0;
+}
---
+++ examples/ConvolutionTest/LossConvexExample1.h
@@ -0,0 +1,22 @@
+//
+// Created by Hui Xie on 6/13/2018.
+//
+
+#ifndef RL_NONCONVEX_LOSSCONVEXEXAMPLE1_H
+#define RL_NONCONVEX_LOSSCONVEXEXAMPLE1_H
+#include "LossLayer.h"
+
+class LossConvexExample1 : public LossLayer {
+public:
+    LossConvexExample1(const int id, const string& name,  Layer *prevLayer);
+    ~LossConvexExample1();
+
+private:
+    virtual float lossCompute();
+    virtual void  gradientCompute();
+    virtual void  printGroundTruth();
+
+};
+
+
+#endif //RL_NONCONVEX_LOSSCONVEXEXAMPLE1_H
