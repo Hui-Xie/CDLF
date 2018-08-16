9053f4dab7c0b74e1b2418e3492b43e801f470e2
Hui Xie <Hui-Xie@uiowa.edu>
2018-08-16 18:57:44 -0500

    refactore ConvolutionTest

---
+++ Examples/ConvolutionTest/ConvNet.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Hui Xie on 8/16/2018.
+// Copyright (c) 2018 Hui Xie. All rights reserved.
+//
+
+#include "ConvNet.h"
---
+++ Examples/ConvolutionTest/ConvNet.h
@@ -0,0 +1,17 @@
+//
+// Created by Hui Xie on 8/16/2018.
+// Copyright (c) 2018 Hui Xie. All rights reserved.
+//
+
+#ifndef CDLF_FRAMEWORK_CONVNET_H
+#define CDLF_FRAMEWORK_CONVNET_H
+
+
+
+class ConvNet {
+
+};
+
+
+
+#endif //CDLF_FRAMEWORK_CONVNET_H
