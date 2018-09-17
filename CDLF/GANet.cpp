66a36b7303d171de83d2de974b8d8300f1e0f7f0
Hui Xie <sheenxh@gmail.com>
2018-09-17 18:06:51 -0500

    add GAN's forwardG

--- CDLF/GANet.cpp
+++ CDLF/GANet.cpp
@@ -16,7 +16,14 @@ GANet::~GANet(){
 }
 
 void GANet::forwardG(){
-
+    for(map<int, Layer*>::iterator iter = m_layers.begin(); iter != m_layers.end(); ++iter){
+        if ("InputLayer" != iter->second->m_type){ // in GAN, original X and groundtruth are both InputLayer
+            iter->second->zeroYTensor();
+        }
+     }
+    for(map<int, Layer*>::iterator iter = m_layers.begin(); iter != m_layers.end(); ++iter){
+        iter->second->forward();
+    }
 }
 
 void GANet::forwardD(){
@@ -39,6 +46,7 @@ void GANet::sgdD(){
 
 }
 
+// in GAN, each layer must belong to G, D or both.
 bool GANet::checkLayerAttribute(){
     for(map<int, Layer*>::iterator iter = m_layers.begin(); iter != m_layers.end(); ++iter){
         if (0 == iter->second->getAttribute()){
