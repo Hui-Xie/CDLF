c3a10dd8079f4171566e71ee4b0ec5624a3e4faf
Hui Xie <sheenxh@gmail.com>
2018-09-20 09:29:49 -0500

    change 3DNet main function

--- Examples/TestSegmentation3D/TestSegmentation3D.cpp
+++ Examples/TestSegmentation3D/TestSegmentation3D.cpp
@@ -11,8 +11,10 @@
 using namespace std;
 
 void printUsage(char* argv0){
-    cout<<"A Generative Adversarial Network for 3D Medical Image Segmentation: "<<endl;
+    cout<<"A Generative Adversarial Network for 3D Medical Images Segmentation: "<<endl;
     cout<<"Usage: "<<endl;
+
+    //todo: add images dirs.
     cout<<argv0<<endl;
 }
 
@@ -26,7 +28,7 @@ int main(int argc, char *argv[])
     Gnet.initialize();
     Gnet.printArchitecture();
 
-    SegmentDNet Dnet("Dicriminative Network");
+    SegmentDNet Dnet("Discriminative Network");
     Dnet.setLearningRate(0.001);
     Dnet.setLossTolerance(0.02);
     Dnet.setBatchSize(3);
@@ -34,24 +36,26 @@ int main(int argc, char *argv[])
     Dnet.printArchitecture();
 
     Segmentation3DNet gan("3DSegmentationGAN", &Gnet,&Dnet);
+    long epochs = 1000;
+
+    // pretrain D:
 
 
-
-
-
-
-
-
-
-    long epoch= 100;
+    // train G, D
+    // quick alternative train
     float accuracy = 0;
-    for (long i=0; i<epoch; ++i){
-        //net.trainG();
-        //accuracy = net.test();
+    for (long i=0; i<epochs; ++i){
+        gan.trainG(1);
+        gan.trainD(1);
+        accuracy = gan.testG();
         cout<<"Epoch_"<<i<<": "<<" accuracy = "<<accuracy<<endl;
     }
+    //slow alternative train
+
+
+
+
+
     cout<<"==========End of Mnist Test==========="<<endl;
     return 0;
-
-
 }
