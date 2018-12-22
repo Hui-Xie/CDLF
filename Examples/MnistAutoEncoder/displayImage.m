6cd16cf1b66b2198da83260dee1509136c576969
Hui Xie <sheenxh@gmail.com>
2018-12-21 19:26:34 -0600

    put corrcoef into image

--- Examples/MnistAutoEncoder/displayImage.m
+++ Examples/MnistAutoEncoder/displayImage.m
@@ -3,22 +3,22 @@
 clear all;
 clc;
 dir = '/home/hxie1/temp_DecoderOutput';
-originFile = strcat(dir, '/T','2-I9','.txt'); %origin input File
-reconstrFile = strcat(dir, '/T','2-R9','.txt'); %Reconstruction file
+originFile = strcat(dir, '/T','3-I5','.txt'); %origin input File
+reconstrFile = strcat(dir, '/T','3-R5','.txt'); %Reconstruction file
 I = dlmread(originFile); R = dlmread(reconstrFile);
 IImage = mat2gray(I,[0,255]); RImage = mat2gray(R,[0,255]);
 
 corrMatrix = corrcoef(reshape(I,784,1), reshape(R, 784, 1));
 corr = corrMatrix(1,2);
 
-displayText = sprintf('Correlation Coefficient: %f', corr);
+displayText = sprintf('\nCorrCoef=%f', corr);
 
 
 [filepath,nameI,ext] = fileparts(originFile);
-subplot(1,2,1); imshow(IImage, []);title(nameI);
+subplot(1,2,1); imshow(IImage, []);title(strcat('Input Image: ',nameI));
 
 [filepath,nameR,ext] = fileparts(reconstrFile);
-subplot(1,2,2); imshow(RImage, []);title(nameR);
+subplot(1,2,2); imshow(RImage, []);title(strcat('Reconstructed Image: ', nameR, displayText));
 
 
 % sgtitle(displayText);
