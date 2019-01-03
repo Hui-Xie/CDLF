07a9f836bb73fc26990bb78f5c10c6b32aa72d9a
Hui Xie <sheenxh@gmail.com>
2019-01-03 13:48:36 -0600

    add AutoEncoder for incorrectly prediction

--- Examples/MnistAutoEncoder/TestMnistAutoEncoder.cpp
+++ Examples/MnistAutoEncoder/TestMnistAutoEncoder.cpp
@@ -9,16 +9,17 @@
 void printUsage(char* argv0){
     cout<<"Test MNIST Dataset AutoEncoder:"<<endl;
     cout<<"Usage: "<<endl;
-    cout<<argv0<<"<netDir> <fullPathOfMnistDataDir> <outputImageDir> <nImages>"<<endl;
+    cout<<argv0<<"<netDir> <fullPathOfMnistDataDir> <outputImageDir> <nImages> <0|1>"<<endl;
     cout<<"For examples: "<<endl;
-    cout<<argv0<<" /home/hxie1/temp_netParameters /home/hxie1/Projects/mnist /home/hxie1/temp_DecoderOutput 400"<<endl;
+    cout<<argv0<<" /home/hxie1/temp_netParameters /home/hxie1/Projects/mnist /home/hxie1/temp_DecoderOutput 40  0"<<endl;
+    cout<<"<0|1> 0: indicate general reconstruction with correct label prediction; 1: indicate only using incorrectly recognized image for reconstruction"<<endl;
 }
 
 
 int main(int argc, char *argv[]){
     printCurrentLocalTime();
     srand (time(NULL));
-    if (5 != argc){
+    if (6 != argc){
         cout<<"Error: input parameter error."<<endl;
         printUsage(argv[0]);
         return -1;
@@ -28,6 +29,8 @@ int main(int argc, char *argv[]){
     const string mnistDir = argv[2];
     const string outputDir = argv[3];
     const int nImages = atoi(argv[4]);
+    const bool usingCorrectPrediction = (0 == atoi(argv[5]))? true: false;
+
 
     CPUAttr cpuAttr;
     cpuAttr.getCPUAttr();
@@ -48,7 +51,7 @@ int main(int argc, char *argv[]){
     //Load MnistAutoEncoder Net
     MnistAutoEncoder net("MnistAutoEncoder", netDir, &mnist);
     if (!isEmptyDir(net.getDir())) {
-        net.load();  //at Dec 11th,2018, the trained G net has an accuracy of 97.11%
+        net.load();  //at Jan  3th,2018, the trained G net has an accuracy of 97.43%
     }
     else{
         cout<<"Error: program can not load a trained Mnist net."<<endl;
@@ -62,15 +65,26 @@ int main(int argc, char *argv[]){
     Tensor<float>  reconstrutImage;
     int  label;
     int  predictLabel;
+    srand (time(NULL));
+
     cout << "Hint: output file format: \n Tindex-IrealLabel.txt, Tindex-RpredictLabel.txt, e.g. T32-R4.txt, T32-P7.txt"<<endl;
     for (int i=0; i<nImages; ++i){
         int index = rand() % 10000;
         mnist.getTestImageAndLabel(index, inputImage, label);
         net.autoEncode(inputImage, predictLabel, reconstrutImage);
-        string filename = outputDir +"/" + "T" + to_string(i)+ "-I"+ to_string(label) +".txt";
+        bool predictCorrectly = (label == predictLabel)? true: false;
+        const string leadingCapital =    predictCorrectly? "T": "F";
+
+        if (!usingCorrectPrediction && predictCorrectly){
+            --i;
+            continue;
+        }
+
+        string filename = outputDir +"/" + leadingCapital + to_string(i)+ "-I"+ to_string(label) +".txt";
         inputImage.save(filename, true);
-        filename = outputDir +"/" + "T" + to_string(i)+ "-R"+ to_string(predictLabel) +".txt";
+        filename = outputDir +"/" + leadingCapital + to_string(i)+ "-R"+ to_string(predictLabel) +".txt";
         reconstrutImage.save(filename,true);
+
     }
     cout << "input files and reconstruction files output at " <<outputDir<<endl;
     cout<< "=========== finished AutoEncoder's image reconstruction ============"<<endl;
