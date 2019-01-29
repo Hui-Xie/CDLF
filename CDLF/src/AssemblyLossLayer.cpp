56f42b8c5b4eb52fbc3318a2d45e6c072dbcd099
Hui Xie <sheenxh@gmail.com>
2019-01-29 14:03:33 -0600

    add Assmebly Loss Layer

---
+++ CDLF/src/AssemblyLossLayer.cpp
@@ -0,0 +1,59 @@
+#include <AssemblyLossLayer.h>
+
+
+AssemblyLossLayer::AssemblyLossLayer(const int id, const string &name, Layer *prevLayer) : LossLayer(id, name, prevLayer) {
+    m_type = "AssemblyLossLayer";
+    m_lossList.clear();
+}
+
+AssemblyLossLayer::~AssemblyLossLayer() {
+    list<LossLayer*>::iterator it = m_lossList.begin();
+    while( it != m_lossList.end()){
+        delete *it;
+        *it = nullptr;
+        ++it;
+    }
+    m_lossList.clear();
+}
+
+void AssemblyLossLayer::addLoss(LossLayer *lossLayer) {
+   m_lossList.push_back(lossLayer);
+}
+
+
+float AssemblyLossLayer::lossCompute() {
+    m_loss = 0.0;
+    list<LossLayer*>::iterator it = m_lossList.begin();
+    while( it != m_lossList.end()){
+        m_loss += (*it)->lossCompute();
+        ++it;
+    }
+    return m_loss;
+}
+
+void AssemblyLossLayer::gradientCompute() {
+    list<LossLayer*>::iterator it = m_lossList.begin();
+    while( it != m_lossList.end()){
+        (*it)->gradientCompute();
+        ++it;
+    }
+}
+
+void AssemblyLossLayer::saveStructLine(FILE *pFile) {
+    //const string tableHead= "ID, Type, Name, previousLayerIDs, outputTensorSize, filterSize, numFilter, FilterStride, startPosition, \r\n";
+    fprintf(pFile, "%d, %s, %s, %d, %s, %s, %d, %f, %s, \r\n", m_id, m_type.c_str(), m_name.c_str(), m_prevLayer->m_id,
+            vector2Str(m_tensorSize).c_str(), "{}", 0, 0, "{}");
+}
+
+void AssemblyLossLayer::printStruct(const int layerIndex) {
+    string assemblyLayer;
+    list<LossLayer*>::iterator it = m_lossList.begin();
+    while( it != m_lossList.end()){
+        assemblyLayer += (*it)->m_type+ " ";
+        ++it;
+    }
+    printf("Layer%03d, Name=%s, Type=%s, id=%d, PrevLayer=%s; Assembly={%s}; \n",
+           layerIndex, m_name.c_str(),m_type.c_str(), m_id,  m_prevLayer->m_name.c_str(), assemblyLayer.c_str());
+}
+
+
--- CDLF/src/Net.cpp
+++ CDLF/src/Net.cpp
@@ -381,6 +381,9 @@ void Net::createLayers(const vector<struct LayerStruct> &layersStructVec) {
         else if ("ReshapeLayer" == s.m_type) {
             pLayer = new ReshapeLayer(s.m_id, s.m_name, pPreLayer, s.m_outputTensorSize);
         }
+        else if ("AssemblyLossLayer" == s.m_type) {
+            pLayer = new AssemblyLossLayer(s.m_id, s.m_name, pPreLayer);
+        }
         else if ("LossConvexExample2" == s.m_type) {
            pLayer = new LossConvexExample2(s.m_id, s.m_name, pPreLayer);
         }
