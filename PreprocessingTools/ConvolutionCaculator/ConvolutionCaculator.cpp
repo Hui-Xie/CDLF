e20bc4799b865a6113fe761bef9298ab925875af
Hui Xie <sheenxh@gmail.com>
2019-02-02 10:06:52 -0600

    according feature dimension, update convolution calculator

--- PreprocessingTools/ConvolutionCaculator/ConvolutionCaculator.cpp
+++ PreprocessingTools/ConvolutionCaculator/ConvolutionCaculator.cpp
@@ -30,22 +30,50 @@ int main(int argc, char *argv[]) {
     const vector<int> filterSize = str2Vector(filterSizeStr);
     const vector<int> stride =  str2Vector(strideStr);
 
-    if (!isElementBiggerThan0(stride)){
-        cout<<"Error: the stride of convolutionLayer should be greater than 0. "<<endl;
+    cout<<"Info: it is a good pratice to keep the output in each dimension odd."<<endl;
+
+    const int dimFilter = filterSize.size();
+    if (dimFilter != stride.size()) {
+        cout << "Error: the dimension of filterSize and stride should be same." << endl;
         return -2;
     }
-    if (filterSize.size() != stride.size() || filterSize.size() != inputTensorSize.size()){
-        cout<<"Error: InputTensorSize, filterSize and stride should have same dims"<<endl;
+
+    if (!isElementBiggerThan0(stride)){
+        cout<<"Error: the stride should be greater than 0. "<<endl;
         return -2;
+    }
+
+    int numInputFeatures  = 0;
+    const int dimX = inputTensorSize.size();
+    if (dimX == dimFilter) {
+        numInputFeatures = 1;
+    } else if (dimX - 1 == dimFilter) {
+        numInputFeatures = inputTensorSize[0];
+    } else {
+        cout<< "Error: the dimensionon of filterSize should be equal with or 1 less than of the dimension of previous layer tensorSize."
+            << endl;
+        return -1;
+    }
+
+    const int s = (1 == numInputFeatures) ? 0 : 1; //indicate whether previousTensor includes feature dimension
+
+
+    for (int i = 0; i < dimFilter; ++i) {
+        if (0 == filterSize[i] % 2 && filterSize[i] !=inputTensorSize[i + s]) {
+            cout << "Error: the filterSize in each dimension should be odd, "
+                    "or if it is even, it must be same size of corresponding dimension of tensorSize of input X."
+                 << endl;
+            return -1;
+        }
     }
 
 
     vector<int> outputTensorSize = inputTensorSize;
-    const int dim = outputTensorSize.size();
-    for (int i = 0; i < dim; ++i) {
-        outputTensorSize[i] = (outputTensorSize[i] - filterSize[i]) / stride[i] + 1;
+    for (int i = 0; i+s < dimX; ++i) {
+        outputTensorSize[i+s] = (outputTensorSize[i+s] - filterSize[i]) / stride[i] + 1;
         // ref formula: http://cs231n.github.io/convolutional-networks/
     }
+
     if (1 != numFilters) {
         outputTensorSize.insert(outputTensorSize.begin(), numFilters);
     }
