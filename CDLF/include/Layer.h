f083126da1087f488b71c7de68e86a427c1ed95b
Hui Xie <sheenxh@gmail.com>
2019-03-18 11:39:16 -0500

    add learningRate notes

--- CDLF/include/Layer.h
+++ CDLF/include/Layer.h
@@ -16,6 +16,30 @@
   #include "LayerCuda.h"
 #endif
 
+/* Learning Rate thinking:
+ *
+ * For every learningRate optimizer, the majority of learning rates fail to train the model.
+ * There is a valley shape for each optimizer: too low a learning rate never progresses, too high a learning rate causes instability and never converges.
+ *    In between there is a band of “just right” learning rates that successfully train.
+ * There is no learning rate that works for all optimizers.
+ * Learning rate can affect training time by an order of magnitude.
+ * ---- abstracted from: https://medium.com/octavian-ai/which-optimizer-and-learning-rate-should-i-use-for-deep-learning-5acb418f9b2
+ *
+ *
+ *  Smith writes, the main assumption behind the rationale for a cyclical learning rate
+ *     is "that increasing the learning rate might have a short term negative effect and yet achieve a longer term beneficial effect."
+ *
+ * increasing the learning rate can also allow for "more rapid traversal of saddle point plateaus."
+ *
+ * A similar approach cyclic approach is known as stochastic gradient descent with warm restarts where an aggressive annealing schedule
+ * is combined with periodic "restarts" to the original starting learning rate.
+ * By drastically increasing the learning rate at each restart, we can essentially exit a local minima and continue exploring the loss landscape.
+ * ---- abstracted from: https://www.jeremyjordan.me/nn-learning-rate/
+ *
+ * */
+
+
+
 //using namespace blaze;
 using namespace std;
 
